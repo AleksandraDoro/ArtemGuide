3ba73eac5b88441584f67a89167dc51489e6609d
AleksandraDoro <aleksandratoys@gmail.com>
2024-02-15 12:36:01 +0300

    Perfect mock's Swagger added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ArtemGuide
+Swagger идеального мока http://localhost:8080/swagger-ui/index.html#/Database/getStudents_1
--- src/main/java/org/example/controllers/StudentController.java
+++ src/main/java/org/example/controllers/StudentController.java
@@ -1,23 +1,34 @@
 package org.example.controllers;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.entity.Student;
 import org.example.service.StudentService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/students")
+
+@Tag(name = "Mock's endpoints",description = "Swagger идеального мока")
 public class StudentController {
     private final StudentService studentService;
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(), mediaType = "application/json")})})
     // возвращает в ответ рандомного студента
     @GetMapping(value = "/random")
     public Student getStudents() {
         return studentService.getStudent();
     }
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(), mediaType = "application/json")})})
     // возвращает в ответ всегда true
     @GetMapping(value = "/true")
     public boolean getBool() {
